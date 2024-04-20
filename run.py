e786d64d5982e6ab429759b2c53f421afb03ca7e
t.cosyn <t.cosyn@groupeonepoint.com>
2024-04-20 23:17:56 +0200

    First commit

--- run.py
+++ run.py
@@ -1,27 +1,29 @@
 import gradio as gr
+import numpy as np
 import plotly.graph_objects as go
-from datasets import load_dataset
 
-dataset = load_dataset("gradio/NYC-Airbnb-Open-Data", split="train")
-df = dataset.to_pandas()
+latitude_range = (48.8167, 48.9026)  # Approximate latitude range for Paris
+longitude_range = (2.25, 2.41)   # Approximate longitude range for Paris
 
-def filter_map(min_price, max_price, boroughs):
 
-    filtered_df = df[(df['neighbourhood_group'].isin(boroughs)) & 
-          (df['price'] > min_price) & (df['price'] < max_price)]
-    names = filtered_df["name"].tolist()
-    prices = filtered_df["price"].tolist()
-    text_list = [(names[i], prices[i]) for i in range(0, len(names))]
+def random_points(nb_points):
+
+    random_coordinates = []
+    for _ in range(nb_points):
+        latitude = np.random.uniform(latitude_range[0], latitude_range[1])
+        longitude = np.random.uniform(longitude_range[0], longitude_range[1])
+        random_coordinates.append((latitude, longitude))
+
     fig = go.Figure(go.Scattermapbox(
-            customdata=text_list,
-            lat=filtered_df['latitude'].tolist(),
-            lon=filtered_df['longitude'].tolist(),
+            customdata=[f"Point {i+1}" for i in range(nb_points)],
+            lat=[coord[0] for coord in random_coordinates],
+            lon=[coord[1] for coord in random_coordinates],
             mode='markers',
             marker=go.scattermapbox.Marker(
-                size=6
+                size=8
             ),
             hoverinfo="text",
-            hovertemplate='<b>Name</b>: %{customdata[0]}<br><b>Price</b>: $%{customdata[1]}'
+            hovertemplate='%{customdata}',
         ))
 
     fig.update_layout(
@@ -30,26 +32,25 @@ def filter_map(min_price, max_price, boroughs):
         mapbox=dict(
             bearing=0,
             center=go.layout.mapbox.Center(
-                lat=40.67,
-                lon=-73.90
+                lat=latitude_range[0] + (latitude_range[1] - latitude_range[0]) / 2,
+                lon=longitude_range[0] + (longitude_range[1] - longitude_range[0]) / 2
             ),
             pitch=0,
-            zoom=9
+            zoom=11
         ),
     )
 
     return fig
 
+
 with gr.Blocks() as demo:
     with gr.Column():
         with gr.Row():
-            min_price = gr.Number(value=250, label="Minimum Price")
-            max_price = gr.Number(value=1000, label="Maximum Price")
-        boroughs = gr.CheckboxGroup(choices=["Queens", "Brooklyn", "Manhattan", "Bronx", "Staten Island"], value=["Queens", "Brooklyn"], label="Select Boroughs:")
-        btn = gr.Button(value="Update Filter")
+            nb_points = gr.Number(value=2, label="Number of points")
+            btn = gr.Button(value="Draw")
         map = gr.Plot()
-    demo.load(filter_map, [min_price, max_price, boroughs], map)
-    btn.click(filter_map, [min_price, max_price, boroughs], map)
+    # demo.load(random_points, [nb_points], map)
+    btn.click(random_points, [nb_points], map)
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(server_name="0.0.0.0")
